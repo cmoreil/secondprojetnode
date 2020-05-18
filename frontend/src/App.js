0e734b6a8bba8755aa33f68fd3fc66e687401c79
MOREIL <caroline.moreil@epitech.eu>
2020-05-18 12:25:35 +0200

    V0 frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+        <p>
+          Mon potager urbain
+        </p>
+
+    </div>
+  );
+}
+
+export default App;
